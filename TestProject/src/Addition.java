06e99647c3d0887e4e188bf35763e2d1754db733
abc <abc@example.com>
2023-11-01 16:11:14 +0530

    add two no

---
+++ TestProject/src/Addition.java
@@ -0,0 +1,15 @@
+
+public class Addition {
+	
+	public static void main(String[] args) {
+		addition(10, 20);
+		
+	}
+	public  static void addition(int a , int b) {
+		
+		int c= a=+b ;
+		System.out.println("value of c>>"+c);
+	}
+
+}
+
